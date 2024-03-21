6c77c6829bff51a4e4818a480cc24cb8a5dcb9a1
David Gorski <gorski.dave@gmail.com>
2024-03-21 15:18:05 -0400

    Add in-memory index

--- src/CommittedStorage.hpp
+++ src/CommittedStorage.hpp
@@ -4,16 +4,34 @@
 #include <string_view>
 #include <optional>
 #include <fstream>
+#include <vector>
+#include <map>
+#include <cstring>
+
+struct Index {
+  char key[30];
+  size_t pos;
+};
 
 class ComittedStorage {
   const std::string _path;
+  std::vector<Index> _index;
 public:
   ComittedStorage(std::string_view path) : _path(path) {}
 
   std::optional<std::string> get(std::string_view key) {
-    std::ifstream scanCommitted(_path, std::ios::binary);
+    auto keySlice = key.substr(0, 29);
+    auto it = std::lower_bound(_index.begin(), _index.end(), keySlice, [](const Index& index, std::string_view key) {
+      return std::string_view(index.key) < key;
+    });
+    if (it == _index.end()) {
+      return std::nullopt;
+    }
+    std::ifstream committed(_path, std::ios::binary);
+    committed.seekg(it->pos);
     std::string line;
-    while (std::getline(scanCommitted, line)) {
+
+    while (std::getline(committed, line)) {
       const auto pos = line.find('\0');
       if (pos != std::string::npos) {
         if (line.substr(0, pos) == key) {
@@ -27,7 +45,7 @@ public:
   template<typename It>
   void add(It begin, It end) {
     // Load current into std::unordered_map
-    std::unordered_map<std::string, std::string> data;
+    std::map<std::string, std::string> data;
     std::ifstream scanCommitted(_path, std::ios::binary);
     std::string line;
     while (std::getline(scanCommitted, line)) {
@@ -45,9 +63,16 @@ public:
       }
     }
 
+    _index.clear();
+
     // Write back to file
     std::ofstream committed(_path, std::ios::binary);
     for (const auto& [key, value] : data) {
+      const auto pos = committed.tellp();
+      auto& currentKeyIndex = _index.emplace_back();
+      std::memcpy(currentKeyIndex.key, key.data(), 29);
+      currentKeyIndex.key[29] = '\0';
+      currentKeyIndex.pos = pos;
       committed << key << '\0' << value << std::endl;
     }
   }
