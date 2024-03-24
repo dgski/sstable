9b1bc413a687bc3179391c122c0bf3a1301ee228
David Gorski <gorski.dave@gmail.com>
2024-03-24 15:19:30 -0400

    Clean-up

--- src/main.cpp
+++ src/main.cpp
@@ -50,16 +50,6 @@ int main(int argc, char* argv[]) {
     db.set(key, value);
   }
 
-  /*
-  const auto commitBenchmark = utils::benchmark([&db] {
-    db.commit();
-    db.prepareCommit();
-    db.commit();
-    return true;
-  }, 1);
-  std::cout << "commitTimeTakenNs=" << commitBenchmark.second << std::endl;
-  */
-
   const auto readsPerfAfterCommit = utils::benchmark([&db, &entries] {
     std::optional<std::string> result;
     for (const auto& [key, value] : entries) {
@@ -71,11 +61,6 @@ int main(int argc, char* argv[]) {
   std::cout
     << "timeForASingleReadAfterCommit=" << timeForASingleReadAfterCommit
     << " result=" << readsPerfAfterCommit.first.value_or("NULL") << std::endl;
-
-  // Erase all
-  for (const auto& [key, value] : entries) {
-    db.remove(key);
-  }
 
   return 0;
 }
